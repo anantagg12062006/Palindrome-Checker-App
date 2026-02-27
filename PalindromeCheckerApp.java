e7a60f769b3cc98ba31635e5dcbc41de4374e482
Anant Aggarwal <aa3867@srmist.edu.in>
2026-02-27 16:03:40 +0530

    :)

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,20 +1,15 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        // Hardcoded string
         String input = "madam";
 
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
+        String reversed = "";
 
-            if (input.charAt(i) != 
-                input.charAt(input.length() - 1 - i)) {
-
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-        if (isPalindrome) {
+
+        if (input.equals(reversed)) {
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is NOT a Palindrome.");
