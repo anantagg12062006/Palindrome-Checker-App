60edbad2ebe052fec0abd3406140f152d0e8e0e9
Anant Aggarwal <aa3867@srmist.edu.in>
2026-02-27 14:41:23 +0530

    msg

--- PalindromeCheckerApp.java
+++
@@ -1,7 +0,0 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Managment System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
-}
-}
