39eeba18181e766580ad637dac3b298b2328750f
Anant Aggarwal <aa3867@srmist.edu.in>
2026-02-27 22:16:16 +0530

    :)

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String input) {
+class PalindromeService {
+    public boolean isPalindrome(String input) {
 
         String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
@@ -18,15 +18,19 @@ public class PalindromeCheckerApp {
 
         return true;
     }
+}
+
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        PalindromeService service = new PalindromeService();
 
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
+        if (service.isPalindrome(input)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
