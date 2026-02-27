d6ba16b71dd3ce0ea04bafcce4fbe0cf5b83ebf9
Anant Aggarwal <aa3867@srmist.edu.in>
2026-02-27 14:49:49 +0530

    :)

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,7 +1,23 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Managment System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
-}
+
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != 
+                input.charAt(input.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+    }
 }
