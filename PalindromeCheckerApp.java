84aff1928e8070b61a48daac46884906aafd2c51
Anant Aggarwal <aa3867@srmist.edu.in>
2026-02-27 16:25:52 +0530

    :)

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
@@ -8,17 +10,24 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         
+    
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+        
         
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
         
         boolean isPalindrome = true;
         
         for (int i = 0; i < input.length(); i++) {
-            char poppedChar = stack.pop();
-            if (input.charAt(i) != poppedChar) {
+            char stackChar = stack.pop();
+            char queueChar = queue.remove();
+            
+            if (stackChar != queueChar) {
                 isPalindrome = false;
                 break;
             }
